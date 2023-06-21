137f64cadbe47ccda6ba382762bd6ccc86da97ff
Tony Scimone <ajscimone@live.com>
2023-06-21 14:45:25 -0600

    Add shell tools and some settings.

--- macOS/setup.sh
+++ macOS/setup.sh
@@ -1,3 +1,49 @@
+# macOS Setup
 
+SCRIPT_FOLDER="$(dirname -- "${BASH_SOURCE[0]}")"
 
-brew install zsh
+# Install Packages
+brew update; brew upgrade; brew cleanup; brew doctor;
+brew install vim curl
+
+# Install git
+brew install git
+echo 'Enter your git email address'
+read email
+echo 'Enter your full name (to appear for commits)'
+read name
+echo 'Enter your preffered default branch name'
+read default_branch
+git config --global user.email $email
+git config --global user.name $name
+git config --global alias.ch checkout
+git config --global alias.s status
+git config --global alias.c commit
+git config --global alias.a add
+git config --global alias.b branch
+git config --global init.defaultBranch default_branch
+
+# Shell Tools
+brew install ripgrep
+brew install bat
+
+# Install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp $SCRIPT_FOLDER/../.zshrc ~/
+
+# VSCode Settings
+cp $SCRIPT_FOLDER/../vscode_settings ~/.config/Code/User/settings.json
+
+# Setup Node.js, nvm, and npm packages
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install 18
+nvm alias default 18
+npm i -g eslint yarn
+
+# Setup C++
+brew install make
+brew install cmake
+brew install llvm
--- wsl/setup.sh
+++ wsl/setup.sh
@@ -1,5 +1,7 @@
 # WSL Setup
 
+SCRIPT_FOLDER="$(dirname -- "${BASH_SOURCE[0]}")"
+
 # Install Packages
 sudo apt -y update
 sudo apt -y full-upgrade
@@ -7,38 +9,10 @@ sudo apt -y clean
 
 sudo apt install -y curl git vim 
 sudo apt install -y build-essential manpages-dev
-sudo apt install python3-pip
-sudo apt install -y python-is-python3
+sudo apt install -y python-is-python3 python3-pip
 
-
-# Install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Setup C++
-sudo apt install -y cmake \
-        cppcheck \
-        cpplint
-sudo apt install -y clang-17 \
-        clang-format-17 \
-        clang-tidy-17 \
-        clang-tools-17 \
-        clangd-17
-
-pip install conan
-
-# Setup Node.js, nvm, and npm packages
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-nvm install 18
-nvm alias default 18
-npm i -g eslint yarn
-
-# Setup Python
-
-
-# Configure git
+# Install git
+sudo apt install -y git 
 echo 'Enter your git email address'
 read email
 echo 'Enter your full name (to appear for commits)'
@@ -53,3 +27,35 @@ git config --global alias.c commit
 git config --global alias.a add
 git config --global alias.b branch
 git config --global init.defaultBranch default_branch
+
+# Shell Tools
+sudo apt install -y ripgrep
+sudo apt install -y bat
+sudo apt install -y fd-find
+
+# Install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp $SCRIPT_FOLDER/../.zshrc ~/
+
+# VSCode Settings
+cp $SCRIPT_FOLDER/../vscode_settings ~/.config/Code/User/settings.json
+
+# Setup C++
+sudo apt install -y cmake \
+        cppcheck \
+        cpplint
+sudo apt install -y clang-17 \
+        clang-format-17 \
+        clang-tidy-17 \
+        clang-tools-17 \
+        clangd-17
+# pip install conan
+
+# Setup Node.js, nvm, and npm packages
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install 18
+nvm alias default 18
+npm i -g eslint yarn
